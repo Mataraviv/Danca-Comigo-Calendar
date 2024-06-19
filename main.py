e2ce3355971df7d9887a40cde7fa5e6a464d8d43
Mataraviv <mataraviv42@gmail.com>
2024-06-19 14:23:44 +0300

    H

--- main.py
+++ main.py
@@ -89,9 +89,9 @@ print('streamlit')
 
 st.title('Studio Availability Checker')
 
-date = st.date_input('Date', value=today_date, min_value=today_date, label_visibility='collapsed')
+date = st.date_input('Date', value=today_date, min_value=today_date, label_visibility='visible')
 start_time = st.time_input('Start Time',time(8, 0),step=900,label_visibility='visible')
-end_time = st.time_input('End Time', time(9, 0),step=900,label_visibility='hidden')
+end_time = st.time_input('End Time', time(9, 0),step=900,label_visibility='collapsed')
 
 
 def valid_end_time_fun(start_time, end_time):
